16c3327189bfca2c00c0485e430c8fa4062accbf
Snit Habtetsion <shabt160@mtroyal.ca>
2026-09-01 17:27:17 -0600

    Added data collection loop and saved raw Korean drama dataset

--- collect_data.py
+++ collect_data.py
@@ -2,17 +2,34 @@ import os
 import requests
 import pandas as pd
 from dotenv import load_dotenv
-
+# Stage 1
 load_dotenv()
 Data = os.getenv('TMDB_API_KEY')
 
 url = 'https://api.themoviedb.org/3/discover/tv'
+dramas = []
 params = {
     'api_key': Data,
-    'with_origin_country': 'KR'
+    'with_origin_country': 'KR',
+    'with_genres' : '18',
+    'sort_by': 'popularity.desc',
+    'page' : 2,
 }
 
-response = requests.get(url, params=params)
-print(response.json())
+# loop through(pagination)
+for page in range (1,21):
+    params['page'] = page
+    response = requests.get(url, params=params)
+    results = response.json()['results']
+    dramas.extend(results)
+    print(f"collected pages:{page}")
+
+print(len(dramas))
+print(dramas[0])
+
+df = pd.DataFrame(dramas)
+os.makedirs("data", exist_ok=True)
+
+df.to_csv("data/raw_dramas.csv")
 
 
